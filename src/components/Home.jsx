33250bc01d8b47ad4a7ab9bbe3d3a73d50b08ee6
PVmendez <valentino.mendez.rey@gmail.com>
2022-09-08 09:46:32 -0300

    Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,18 @@
-import About from "./HomeComponents/About";
-import Carousel from "./HomeComponents/Carousel";
-import Filter from "./HomeComponents/Filter";
-import Footer from "./HomeComponents/Footer";
-import Navbar from "./HomeComponents/Navbar";
-import Popular from "./HomeComponents/Popular";
+import About from "./HomeComponents/About/About";
+import Carousel from "./HomeComponents/Carousel/Carousel";
+import Filter from "./HomeComponents/Filter/Filter";
+import Footer from "./HomeComponents/Footer/Footer";
+import Navbar from "./HomeComponents/Navbar/Navbar";
+import "./HomeComponents/Home.css";
 
 import React from "react";
 
 export default function Home() {
   return (
-    <div>
+    <div className="Home">
       <Navbar />
       <Carousel />
       <Filter />
-      <Popular />
       <About />
       <Footer />
     </div>
--- src/components/HomeComponents/Navbar/Navbar.jsx
+++ src/components/HomeComponents/Navbar/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div class="navbar navbar-expand-lg bg-light navbar-light ">
+    <div class="container-fluid">
+      <Link to="/" class="navbar-brand">
+        <picture>
+          <img src="logo192.png" alt="" />
+        </picture>
+      </Link>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+        <div class="navbar-nav ml-auto">
+          <Link to="" class="nav-item nav-link">FABRICACIÓN</Link>
+          <Link to="" class="nav-item nav-link">PRODUCTOS</Link>
+          <Link to="/" class="nav-item nav-link">PRESENTACIONES</Link>
+          <Link to="/" class="nav-item nav-link">LA EMPRESA</Link>
+          <Link to="/" class="nav-item nav-link">CONTACTO</Link>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+
+
   )
 }
