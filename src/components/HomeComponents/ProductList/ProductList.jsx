035052bf08b1d15d6cd419635bdb5af2457482d9
F-Arzuaga <108815637+F-Arzuaga@users.noreply.github.com>
2022-09-09 01:06:44 -0300

    product list y filter

--- src/components/HomeComponents/ProductList/ProductList.jsx
+++ src/components/HomeComponents/ProductList/ProductList.jsx
@@ -3,10 +3,30 @@ import { Product } from "./Product";
 import Navbar from "../../Navbar/Navbar";
 import Footer from "../../Footer/Footer";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const ProductList = () => {
+  const [productos, setProductos] = useState();
+  const [category, setCategory] = useState(0);
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_BASE_URL);
+    const getByCategory = async () => {
+      const result = await axios({
+        method: "get",
+        baseURL: `${process.env.REACT_APP_API_BASE_URL}`,
+        url: `/products/category/${category}`,
+      });
+      setProductos(result.data);
+    };
+    getByCategory();
+  }, [category]);
+
   return (
     <>
+      {productos && console.log(productos)}
       <Navbar />
       <header className="page-section masthead2">
         <div className="container h-50">
@@ -29,95 +49,55 @@ export const ProductList = () => {
               </h2>
               <hr />
 
-              <p className="blog-sidebar-list">
+              <p
+                className="blog-sidebar-list"
+                onClick={() => {
+                  setCategory(0);
+                }}
+              >
                 <b>
-                  <span className="list-icon"> {">"} </span> ...
+                  <span className="list-icon"> {">"} </span> Todos
                 </b>
               </p>
-              <p className="blog-sidebar-list">
+              <p
+                className="blog-sidebar-list"
+                onClick={() => {
+                  setCategory(1);
+                }}
+              >
                 <b>
-                  <span className="list-icon"> {">"} </span> ...
+                  <span className="list-icon"> {">"} </span> Clasicos
                 </b>
               </p>
-              <p className="blog-sidebar-list">
+              <p
+                className="blog-sidebar-list"
+                onClick={() => {
+                  setCategory(2);
+                }}
+              >
                 <b>
-                  <span className="list-icon"> {">"} </span> ...
+                  <span className="list-icon"> {">"} </span> Premium
                 </b>
               </p>
 
               <div>&nbsp;</div>
               <div>&nbsp;</div>
-
-              <h2 className="blog-sidebar-title">
-                <b>FILTRO</b>
-              </h2>
-              <hr />
-
-              <div className="input-group mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search"
-                  aria-label="Receipient's username"
-                />
-                <div className="input-group-append">
-                  <span className="input-group-text" id="basic-addon2">
-                    <i className="fa fa-search"></i>
-                  </span>
-                </div>
-              </div>
-              <label htmlFor="customRange1" className="form-label">
-                Chosen price range
-              </label>
-              <input type="range" className="form-range" id="customRange1" />
-              <button type="button" className="btn btn-dark btn-lg">
-                FILTRO
-              </button>
-
-              <div>&nbsp;</div>
-              <div>&nbsp;</div>
-              <h2 className="blog-sidebar-title">
-                <b>Tags</b>
-              </h2>
-              <p className="product-sidebar-list">
-                <b>#accessory, #arabica</b>
-              </p>
-              <p className="product-sidebar-list">
-                <b>#candy, #bean, #cup</b>
-              </p>
-              <p className="product-sidebar-list">
-                <b>#ethopian, #chocolatte</b>
-              </p>
             </div>
 
             <div className="col-lg-9">
               <div className="row">
-                <div className="col">Showing all 9 results</div>
-
                 <div className="col">
-                  <select className="form-control">
-                    <option value="">Default Sorting</option>
-                    <option value="popularity">Sorting by popularity</option>
-                    <option value="average">Sorting by average</option>
-                    <option value="latest">Sorting by latest</option>
-                    <option value="low">Sorting by low</option>
-                    <option value="high">Sorting by high</option>
-                  </select>
+                  Showing all {productos.length} results
                 </div>
               </div>
               <div>&nbsp;</div>
               <div>&nbsp;</div>
 
               <div className="row justify-content-center">
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
+                {productos &&
+                  productos.map((producto) => {
+                    return <Product producto={producto} />;
+                  })}
               </div>
             </div>
           </div>
