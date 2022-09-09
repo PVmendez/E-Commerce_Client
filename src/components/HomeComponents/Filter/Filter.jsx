217c3940f0a5d5eb561b422140f4bc3d27b1d05f
Sofia Valdez <valdezsofia7@gmail.com>
2022-09-09 01:01:09 -0300

    ProductDetail y destacados

--- src/components/HomeComponents/Filter/Filter.jsx
+++ src/components/HomeComponents/Filter/Filter.jsx
@@ -3,19 +3,19 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Filter() {
-  const [popularProducts, setPopularProducts] = useState(null);
+  const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(() => {
     const getPopularProducts = async () => {
       const result = await axios({
         method: "GET",
-        url: "http://localhost:8000/products/popular",
+        baseURL: process.env.REACT_APP_API_BASE_URL,
+        url: "/products/popular",
       });
-      console.log(result.data)
+      console.log(result.data);
       setPopularProducts(result.data);
     };
     getPopularProducts();
-    
   }, []);
 
   return (
@@ -32,17 +32,38 @@ export default function Filter() {
           <p className="p-filter">Esto es lo que hacemos.</p>
         </div>
       </div>
-      <div className="col-12 d-flex justify-content-center p-0 flex-column flex-lg-row align-items-center">
-        <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2 d-flex justify-content-center">
-          <img className="img-filter" src="../img/Bombon_1.jpg" alt="bombon1" />
-        </div>
-        <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2 d-flex justify-content-center">
-          <img className="img-filter" src="../img/Bombon_2.jpg" alt="bombon2" />
-        </div>
-        <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2 d-flex justify-content-center">
-          <img className="img-filter" src="../img/Bombon_3.jpg" alt="bombon3" />
-        </div>
-      </div>
+      {popularProducts.map((popularProduct, index) => {
+        return (
+          <>
+            <div className="col-12 d-flex justify-content-center p-0 flex-column flex-lg-row align-items-center">
+              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2 d-flex justify-content-center">
+                <img
+                  key={index}
+                  className="img-filter"
+                  src={`img/${popularProduct.image}`}
+                  alt="bombon1"
+                />
+              </div>
+              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2 d-flex justify-content-center">
+                <img
+                  key={index}
+                  className="img-filter"
+                  src={`img/${popularProduct.image}`}
+                  alt="bombon2"
+                />
+              </div>
+              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2 d-flex justify-content-center">
+                <img
+                  key={index}
+                  className="img-filter"
+                  src={`img/${popularProduct.image}`}
+                  alt="bombon3"
+                />
+              </div>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
