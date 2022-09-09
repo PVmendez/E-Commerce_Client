d0f92c42472648200440d6b80e3b7f939ca7cbcb
F-Arzuaga <108815637+F-Arzuaga@users.noreply.github.com>
2022-09-09 10:02:50 -0300

    arreglos adicionales

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,40 +4,40 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div class="navbar navbar-expand-lg navbar-light navBar">
-      <div class="container-fluid container-nav">
-        <Link to="/" class="navbar-brand">
+    <div className="navbar navbar-expand-lg navbar-light navBar">
+      <div className="container-fluid container-nav">
+        <Link to="/" className="navbar-brand">
           <picture>
             <img src="img/logo.png" alt="" />
           </picture>
         </Link>
         <button
           type="button"
-          class="navbar-toggler"
+          className="navbar-toggler"
           data-toggle="collapse"
           data-target="#navbarCollapse"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
         <div
-          class="collapse navbar-collapse justify-content-end"
+          className="collapse navbar-collapse justify-content-end"
           id="navbarCollapse"
         >
-          <div class="navbar-nav ml-auto">
-            <Link to="" class="nav-item nav-link">
+          <div className="navbar-nav ml-auto">
+            <Link to="" className="nav-item nav-link">
               FABRICACIÓN
             </Link>
-            <Link to="" class="nav-item nav-link">
+            <Link to="" className="nav-item nav-link">
               PRODUCTOS
             </Link>
-            <Link to="/" class="nav-item nav-link">
+            <Link to="/" className="nav-item nav-link">
               PRESENTACIONES
             </Link>
-            <Link to="/" class="nav-item nav-link">
+            <Link to="/" className="nav-item nav-link">
               LA EMPRESA
             </Link>
-            <Link to="/" class="nav-item nav-link">
+            <Link to="/" className="nav-item nav-link">
               CONTACTO
             </Link>
           </div>
