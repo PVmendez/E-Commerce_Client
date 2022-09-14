4f046ef0b47e928c953f37b945b5aa45cc1b6b98
Sofia Valdez <valdezsofia7@gmail.com>
2022-09-13 22:03:50 -0300

    Checkout

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Payment.css";
 import axios from "axios";
@@ -14,10 +14,14 @@ export default function Payment() {
   const [city, setCity] = useState("");
   const [postalCode, setPostalCode] = useState("");
   const [number, setNumber] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [subTotalPrice, setSubTotalPrice] = useState("");
+  const [totalPrice, setTotalPrice] = useState(0);
   const navigate = useNavigate();
   const cartStore = useSelector((state) => state.cart);
   const userStore = useSelector((state) => state.user);
   const dispatch = useDispatch();
+
   const payOrder = () => {
     const postCart = async () => {
       const result = await axios({
@@ -29,7 +33,7 @@ export default function Payment() {
           Authorization: `Bearer ${userStore[0].token}`,
         },
       });
-      console.log(result.status);
+      console.log(result.data);
       if (result.status === 201) {
         navigate("/");
         dispatch(emptyCart());
@@ -38,99 +42,174 @@ export default function Payment() {
     postCart();
   };
 
+  useEffect(() => {
+    const sumQuantity = cartStore
+      .map((item) => item.quantity)
+      .reduce((prev, curr) => prev + curr, 0);
+    setQuantity(sumQuantity);
+  }, []);
+
+  useEffect(() => {
+    setSubTotalPrice(0);
+    for (let i = 0; i < cartStore.length; i++) {
+      setSubTotalPrice((prev) => {
+        return prev + cartStore[i].product.price * cartStore[i].quantity;
+      });
+    }
+  }, [cartStore]);
+
+  useEffect(() => {
+    setTotalPrice(() => {
+      return subTotalPrice + (1.5 + 2.5);
+    });
+  }, []);
+
   return (
-    <div className="container">
+    <div className="container" id="container-checkout">
       <div className="row ">
         <div className="navbar-checkout d-flex mt-5 mb-5">
           <h4>Finalizar compra</h4>
 
           <Link to="/productos">
-            <p>Seguir comprando</p>
+            <p className="m-0">Seguir comprando</p>
           </Link>
         </div>
       </div>
+
       <div className="row main-checkout">
-        <h2>1 Datos de envio</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            payOrder();
-          }}
-        >
-          <div className="mb-4">
-            <input
-              type="text"
-              className="form-control form-checkout"
-              id="nombre"
-              onChange={(e) => setFirstName(e.target.value)}
-              value={firstname}
-              placeholder="Nombre"
-            ></input>
+        <div className="col-6">
+          <h2>1 Datos de envio</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              payOrder();
+            }}
+          >
+            <div className="mb-4">
+              <input
+                type="text"
+                className="form-control form-checkout"
+                id="nombre"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstname}
+                placeholder="Nombre"
+              ></input>
+            </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                className="form-control form-checkout"
+                id="apellido"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastname}
+                placeholder="Apellido"
+              ></input>
+            </div>
+            <div className="mb-4">
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                className="form-control form-checkout"
+                id="email"
+                value={email}
+                placeholder="Email"
+              ></input>
+            </div>
+            <div className="mb-4">
+              <input
+                onChange={(e) => setAddress(e.target.value)}
+                type="text"
+                className="form-control form-checkout"
+                id="direccion"
+                value={address}
+                placeholder="Dirección"
+              ></input>
+            </div>
+            <div className="mb-4">
+              <input
+                onChange={(e) => setCity(e.target.value)}
+                type="text"
+                className="form-control form-checkout"
+                id="ciudad"
+                value={city}
+                placeholder="Ciudad"
+              ></input>
+            </div>
+            <div className="mb-4">
+              <input
+                onChange={(e) => setPostalCode(e.target.value)}
+                type="text"
+                className="form-control form-checkout"
+                id="codigoPostal"
+                value={postalCode}
+                placeholder="Codigo postal"
+              ></input>
+            </div>
+            <div className="mb-4">
+              <input
+                onChange={(e) => setNumber(e.target.value)}
+                type="text"
+                className="form-control form-checkout"
+                id="telefono"
+                value={number}
+                placeholder="Número de teléfono"
+              ></input>
+            </div>
+            <button type="submit" className="button-filter button-checkout">
+              Confirmar pedido
+            </button>
+          </form>
+        </div>
+        <div className="col-6">
+          <div className="box-cartCheckout">
+            <div className="d-flex justify-content-between ms-3">
+              <p className="summary">Resumen del pedido({quantity})</p>
+              <Link to="/carrito"> Editar el carrito</Link>
+            </div>
+
+            <div className="productsPayment">
+              {cartStore.map((item) => {
+                return (
+                  <div className="d-flex justify-content-between">
+                    <div className="d-flex align-items-center">
+                      <img
+                        className="imagenPayment"
+                        src={`./img/${item.product.image}`}
+                        alt="imagenProducto"
+                      />
+                      <div>
+                        <p className="m-0" style={{ fontWeight: 600 }}>
+                          {item.product.name}
+                        </p>
+                        <p className="m-0">Cantidad: {item.quantity}</p>
+                      </div>
+                    </div>
+                    <div className="d-flex ">
+                      <p className="align-self-center m-0">
+                        U$D {item.product.price * item.quantity}
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="pt-2  pb-3 promocion">
+              <Link to="/">
+                <i className="fas fa-tag fa-1x ms-3 me-2 mt-2 "></i>Ingresa un
+                código de promoción
+              </Link>
+            </div>
+            <div className="prices mt-3 ms-3">
+              <p>Subtotal: U$D {subTotalPrice}</p>
+              <p>Envío: U$D 1.50</p>
+              <p>Impuestos: U$D 2.50</p>
+            </div>
+            <div className="totalPrice ms-3 mt-3">
+              <p>Total</p>
+              U$D{totalPrice}
+            </div>
           </div>
-          <div className="mb-4">
-            <input
-              type="text"
-              className="form-control form-checkout"
-              id="apellido"
-              onChange={(e) => setLastName(e.target.value)}
-              value={lastname}
-              placeholder="Apellido"
-            ></input>
-          </div>
-          <div className="mb-4">
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              className="form-control form-checkout"
-              id="email"
-              value={email}
-              placeholder="Email"
-            ></input>
-          </div>
-          <div className="mb-4">
-            <input
-              onChange={(e) => setAddress(e.target.value)}
-              type="text"
-              className="form-control form-checkout"
-              id="direccion"
-              value={address}
-              placeholder="Dirección"
-            ></input>
-          </div>
-          <div className="mb-4">
-            <input
-              onChange={(e) => setCity(e.target.value)}
-              type="text"
-              className="form-control form-checkout"
-              id="ciudad"
-              value={city}
-              placeholder="Ciudad"
-            ></input>
-          </div>
-          <div className="mb-4">
-            <input
-              onChange={(e) => setPostalCode(e.target.value)}
-              type="text"
-              className="form-control form-checkout"
-              id="codigoPostal"
-              value={postalCode}
-              placeholder="Codigo postal"
-            ></input>
-          </div>
-          <div className="mb-4">
-            <input
-              onChange={(e) => setNumber(e.target.value)}
-              type="text"
-              className="form-control form-checkout"
-              id="telefono"
-              value={number}
-              placeholder="Número de teléfono"
-            ></input>
-          </div>
-          <button type="submit" className="button-filter button-checkout">
-            Confirmar pedido
-          </button>
-        </form>
+        </div>
       </div>
     </div>
   );
