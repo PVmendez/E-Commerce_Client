23ff2198c16c89fe8c0efb419170d6a113c512ba
Sofia Valdez <valdezsofia7@gmail.com>
2022-09-20 15:10:06 -0300

    Pagina de error

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import "./ErrorPage.css"
+import "./ErrorPage.css";
 
 export const ErrorPage = () => {
   return (
     <>
-    <Navbar />
-      <div className="main_body">
-        <div className="center_body">
-          <h1>404</h1>
-          <h2>PAGE NOT FOUND</h2>
-          <Link to="/">
-            {" "}
-            go to homepage
+      <Navbar />
+      <div className="errorRow">
+        <div className="main_body text-center mt-5">
+          <div className="center_body">
+            <h1>404</h1>
+            <h2>Página no encontrada </h2>
+            <Link to="/">
+              Te invitamos seguir experiementando sabores únicos
+            </Link>
+          </div>
+          <Link to="/" className="button-pageNotFound">
+            Volver
           </Link>
         </div>
       </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
